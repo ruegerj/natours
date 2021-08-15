826025bea1a9a30ff17fbd18ef52ee8217a895c2
ruegerj <jan.rueger@gmx.ch>
2021-08-15 21:11:28 +0200

    Add response compression
    Adds the 'compression' package in order to compress
    any suitable response data.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -87,6 +88,9 @@ app.use(
     ],
   })
 );
+
+// Use response compression
+app.use(compression());
 
 // Mount routers
 app.use('/', viewRouter);
